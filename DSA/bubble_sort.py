be307d84e16a13843568726327dd974d2806c6f6
Rudra <rudra2462002@gmail.com>
2024-09-17 15:24:31 +0530

    DSA

--- DSA/bubble_sort.py
+++ DSA/bubble_sort.py
@@ -1,13 +1,22 @@
 def Bubble_Sort(A):
     n = len(A)
-    N = n-1
-    for j in range(0,n):
-        for i in range(0,N):
-            if A[i] > A[i+1]:
-                A[i],A[i+1]=A[i+1],A[i]
-        N=N-1
+    N = n - 1
+    for j in range(0, n):
+        for i in range(0, N):
+            if A[i] > A[i + 1]:
+                A[i], A[i + 1] = A[i + 1], A[i]
+        N = N - 1
     return A
 
-#main
-arr = [21,43,24,23,66,74,55,97,34,99]
-print("sorted array is :",Bubble_Sort(arr))
+
+# main
+arr = [21, 43, 24, 23, 66, 74, 55, 97, 34, 99]
+print("sorted array is :", Bubble_Sort(arr))
+
+# we change the n
+# ..................
+# ...n = len(A)
+# for j in range(n):
+#     for i in range(0, n-i-1):
+#         if A[i] > A[i + 1]:.....
+#         .....further code
